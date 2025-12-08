982d46689b33409956dbf6ea46cfc8d762b79acf
msvantesson <svante01@yahoo.com>
2025-12-08 11:20:14 +0100

    Update supabaseClient.ts

--- app/lib/supabaseClient.ts
+++ app/lib/supabaseClient.ts
@@ -4,6 +4,6 @@ import { createClient, SupabaseClient } from '@supabase/supabase-js'
 export function getSupabaseClient(): SupabaseClient {
   return createClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY! // must be anon key
   )
 }
