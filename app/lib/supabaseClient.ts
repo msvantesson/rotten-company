b18ac59e6c3e225bbc089a9d75234e84672c36d7
msvantesson <svante01@yahoo.com>
2025-12-08 11:50:34 +0100

    Update supabaseClient.ts

--- app/lib/supabaseClient.ts
+++ app/lib/supabaseClient.ts
@@ -1,5 +1,8 @@
+// app/lib/supabaseClient.ts
 import { createClient } from '@supabase/supabase-js'
 
+// Create a single supabase client for the entire app
 export const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY! 
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+)
