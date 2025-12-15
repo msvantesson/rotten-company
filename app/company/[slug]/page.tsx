396a6ec30a3580c69a0cc4206029f3b350e76435
msvantesson <svante01@yahoo.com>
2025-12-15 10:38:33 +0100

    Update page.tsx

--- app/company/[slug]/page.tsx
+++ app/company/[slug]/page.tsx
@@ -1,6 +1,7 @@
+export const dynamic = "force-dynamic";
 import { notFound } from "next/navigation";
 import { supabase } from "@/lib/supabaseClient";
-export const dynamic = "force-dynamic";
+
 
 
 export default async function CompanyPage({ params }: { params: { slug: string } }) {
