d66f3f61741fa8eb8fef5ccf9024fc79c79ff123
msvantesson <svante01@yahoo.com>
2025-12-17 15:16:55 +0100

    Update page.tsx

--- app/submit-company/page.tsx
+++ app/submit-company/page.tsx
@@ -1,35 +1,9 @@
-// app/submit-company/page.tsx
-
 import SubmitCompanyForm from '@/components/SubmitCompanyForm';
 
 export default function SubmitCompanyPage() {
   return (
     <div className="p-6 max-w-2xl mx-auto">
       <SubmitCompanyForm />
-    </div>
-  );
-}
-
-
-
-
-
-
-export default async function SubmitCompanyPage() {
-  const supabase = createClient();
-  const { data: { user } } = await supabase.auth.getUser();
-
-  if (!user) {
-    return (
-      <div className="p-6">
-        <h2 className="text-xl font-bold">You must be logged in to submit a company.</h2>
-      </div>
-    );
-  }
-
-  return (
-    <div className="p-6 max-w-2xl mx-auto">
-      <SubmitCompanyForm user={user} />
     </div>
   );
 }
