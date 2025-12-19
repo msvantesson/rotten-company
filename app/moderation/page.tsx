2eb30a980a36924d722f9f90fe7d88fac118d06c
msvantesson <svante01@yahoo.com>
2025-12-19 13:56:30 +0100

    Update page.tsx

--- app/moderation/page.tsx
+++ app/moderation/page.tsx
@@ -1,4 +1,5 @@
-export const dynamic = "force-dynamic";
+export const dynamic = "force-dynamic"; 
+export const revalidate = 0;
 
 import { supabase } from "@/lib/supabaseClient";
 import { revalidatePath } from "next/cache";
