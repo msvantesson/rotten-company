bb80d799a98b985d45db99ddb84efe4c4efe0d96
msvantesson <svante01@yahoo.com>
2025-12-29 14:55:31 +0100

    Update supabase-server.ts

--- lib/supabase-server.ts
+++ lib/supabase-server.ts
@@ -1,11 +1,15 @@
 // /lib/supabase-server.ts
-import { createServerClient } from "@supabase/auth-helpers-nextjs";
 import { cookies } from "next/headers";
+import { createServerClient } from "@supabase/auth-helpers-nextjs";
 
-export function createServerSupabase() {
+export function supabaseServer() {
+  const cookieStore = cookies(); // must CALL cookies()
+
   return createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.SUPABASE_SERVICE_ROLE_KEY!, // safe on server
-    { cookies }
+    {
+      cookies: cookieStore, // pass the cookie object, not the function
+    }
   );
 }
