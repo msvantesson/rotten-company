3c19b75398380b2e4efe59a62599af463c68e8f6
msvantesson <svante01@yahoo.com>
2025-12-29 13:15:55 +0100

    Update page.tsx

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,11 +1,14 @@
+// /app/login/page.tsx
 "use client";
 
 import { useState } from "react";
 import { useRouter } from "next/navigation";
-import { supabase } from "@/lib/supabaseClient";
+import { createClient } from "@/lib/supabase-client";
 
 export default function LoginPage() {
   const router = useRouter();
+  const supabase = createClient();
+
   const [mode, setMode] = useState<"login" | "signup">("login");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
