57473b71316acb1b270512e4a5bffda2484b314c
msvantesson <svante01@yahoo.com>
2025-12-29 14:01:31 +0100

    Update supabaseClient.ts

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -1,9 +1,7 @@
-// /lib/supabase-client.ts
+// /lib/supabaseClient.ts
 import { createBrowserClient } from "@supabase/auth-helpers-nextjs";
 
-export function createClient() {
-  return createBrowserClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-  );
-}
+export const supabase = createBrowserClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL!,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+);
