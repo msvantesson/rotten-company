25ea117f63bf8f5997d239a6d1ce4e1ff443901a
msvantesson <svante01@yahoo.com>
2026-01-02 14:11:23 +0100

    Update supabase-route.ts

--- lib/supabase-route.ts
+++ lib/supabase-route.ts
@@ -4,7 +4,8 @@ import { cookies } from "next/headers";
 import { createServerClient } from "@supabase/ssr";
 
 export function supabaseRoute() {
-  const cookieStore = cookies();
+const cookieStore = await cookies();
+
 
   return createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
