c44940cfc82387ecef5307bf687caaffc78fac8f
msvantesson <svante01@yahoo.com>
2026-01-08 13:34:55 +0100

    Add CountrySelectClient component for country selection

---
+++ app/rotten-index/CountrySelectClient.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import React from "react";
+
+type Option = { dbValue: string; label: string };
+
+export default function CountrySelectClient({
+  id,
+  name,
+  value,
+  options,
+}: {
+  id?: string;
+  name: string;
+  value?: string | null;
+  options: Option[];
+}) {
+  return (
+    <select
+      id={id}
+      name={name}
+      defaultValue={value || ""}
+      onChange={(e) => {
+        const form = (e.target as HTMLSelectElement).form;
+        if (form) form.submit();
+      }}
+      className="border border-gray-300 rounded px-2 py-1 text-sm bg-white"
+    >
+      <option value="">All countries</option>
+      {options.map((opt) => (
+        <option key={opt.dbValue} value={opt.dbValue}>
+          {opt.label}
+        </option>
+      ))}
+    </select>
+  );
+}
