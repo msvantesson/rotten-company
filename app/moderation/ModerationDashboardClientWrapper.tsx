cee3527d93cc70bafe6a387d5c60e985d42f309f
msvantesson <svante01@yahoo.com>
2026-01-13 13:10:19 +0100

    Delete app/moderation/ModerationDashboardClientWrapper.tsx

--- app/moderation/ModerationDashboardClientWrapper.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-import dynamic from "next/dynamic";
-
-const ModerationDashboardClient = dynamic(() => import("./ModerationDashboardClient"), {
-  ssr: false,
-});
-
-export default ModerationDashboardClient;
