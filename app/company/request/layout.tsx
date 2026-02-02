b6bbaaad0d3203badb1c615646df6115b6c4e24a
msvantesson <svante01@yahoo.com>
2026-02-02 09:48:30 +0100

    Refactor RequestLayout component syntax

--- app/company/request/layout.tsx
+++ app/company/request/layout.tsx
@@ -1,7 +1,9 @@
 "use client";
 
-export const dynamic = "force-dynamic";
-
-export default function RequestLayout({ children }: { children: React.ReactNode }) {
+export default function RequestLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return <>{children}</>;
 }
