776d44ba3b21b1656c2d4cda08aae6d6932fb813
msvantesson <svante01@yahoo.com>
2026-02-09 14:57:48 +0100

    Update page.tsx

--- app/my-evidence/[id]/page.tsx
+++ app/my-evidence/[id]/page.tsx
@@ -1,11 +1,17 @@
 export default function MyEvidencePage({
   params,
 }: {
-  params: Record<string, string>;
+  params: { id: string };
 }) {
   return (
     <pre style={{ padding: 24 }}>
-{JSON.stringify(params, null, 2)}
+{JSON.stringify(
+  {
+    id: params.id,
+  },
+  null,
+  2
+)}
     </pre>
   );
 }
