74d4a2ea4630d67d3c84f779b4e76d35e0817954
msvantesson <svante01@yahoo.com>
2026-03-18 08:49:48 +0100

    Change eventId type from number to string

--- app/page.tsx
+++ app/page.tsx
@@ -13,7 +13,7 @@ function formatDate(dateStr: string): string {
 }
 
 type RecentlyVerifiedItem = {
-  eventId: number;
+  eventId: string;
   createdAt: string;
   evidenceTitle: string;
   companyName: string;
